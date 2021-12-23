7d073f36c2cc1282878253558b9a45666feb1dec
Tejas Patil <95238373+Tejas2305@users.noreply.github.com>
2021-12-23 10:21:22 +0530

    Add files via upload

---
+++ division.c
@@ -0,0 +1,10 @@
+void main()
+{
+    int a,b,c;
+    printf("Enter fist number:");
+    scanf("%d",&a);
+    printf("Enter second number:");
+    scanf("%d",&b);
+    c=a/b;
+    printf("%d/%d=%d",a,b,c);
+}
